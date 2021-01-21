b05c81d2b25712d5293d60673d0421dbcd9c90ca
MariamKiki <69806593+MariamKiki@users.noreply.github.com>
2021-01-22 03:04:41 +0400

    initial fk future

---
+++ assets/js/scripts.js
@@ -0,0 +1,17 @@
+/*!
+ * atomic
+ * Atomic Guide Builder
+ * https://gitlab.com/kishmariageorge/atomic-guide-builder.git
+ * @author George Kishmareia
+ * @version 1.0.0
+ * Copyright 2021. MIT licensed.
+ */
+(function ($, window, document, undefined) {
+
+  'use strict';
+
+  $(function () {
+    // Atomic
+  });
+
+})(jQuery, window, document);
